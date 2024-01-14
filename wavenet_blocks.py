Fixed causal conv 1d block

wavenet_blocks.py
@@ -21,12 +21,13 @@ class CausalConvBlock(nn.Module):
     super(CausalConvBlock, self).__init__()
     self.layers = layers
     self.in_channels = in_channels
-    self.out_channels = out_channels
+    self.final_out_channels = out_channels
     self.kernel_size = kernel_size
     self.module_list = nn.ModuleList()
 
-    for i in range(layers):
-      self.module_list.append(CausalConv1d(in_channels,out_channels,kernel_size,dilation=1))
+    for i in range(len(layers)-1):
+      in_channel,out_channel = layers[i], layers[i+1]
+      self.module_list.append(CausalConv1d(in_channel,out_channel,kernel_size,dilation=1)) #maybe batchnorm here?
 
   def forward(self,x):
     for layer in self.module_list:

wavenet_model.py
@@ -7,7 +7,7 @@ from wavenet_blocks import *
 class WaveNetModel(nn.Module):
   def __init__(self,in_channels,out_channels,kernel_size,residual_blocks):
     super(WaveNetModel, self).__init__()
-    self.causal_conv1d_layers = 5
+    self.causal_conv1d_layers = [in_channels//(2**i) for i in range(5)] + [1]
     self.dilated_block_layers = 10
     self.in_channels = in_channels
     self.out_channels = out_channels